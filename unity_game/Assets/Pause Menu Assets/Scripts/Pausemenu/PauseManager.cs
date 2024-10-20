17e7f18c67e7a71020fc24a1222bd3c7e09e52c6
projekt00 <projektunity684@gmail.com>
2024-10-20 18:43:27 +0200

    PauseMenu
    wywalam pause menu bu nie działa

--- unity_game/Assets/Pause Menu Assets/Scripts/Pausemenu/PauseManager.cs
+++ unity_game/Assets/Pause Menu Assets/Scripts/Pausemenu/PauseManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.IO;
-using UnityEditor.Events;
+using UnityEngine.Events;
 using static UnityEngine.InputSystem.PlayerInput;
 using UnityEngine.Events;
 //using UnityStandardAssets.ImageEffects;
